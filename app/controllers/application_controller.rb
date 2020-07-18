d0ce5b7b8e55c994d466d4e7874a479b39d86ad5
karajan-37458 <66232530+karajan-37458@users.noreply.github.com>
2020-07-18 18:20:37 +0900

    Upgrade actionpack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 
   protected
 
